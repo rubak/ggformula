b122c62bde08cf85c8ee00bcc012a9289511f1dd
Randall Pruim <rpruim@gmail.com>
2017-07-15 09:38:12 -0400

    updating documentation

--- R/gf_functions.R
+++ R/gf_functions.R
@@ -365,9 +365,25 @@ gf_path <-
 #'   gf_smooth(births ~ date, color = ~wday, data = Births78)
 #'   gf_smooth(births ~ date, color = ~wday, data = Births78, fullrange = TRUE)
 #'   gf_smooth(births ~ date, color = ~wday, data = Births78, show.legend = FALSE, se = FALSE)
-#'   gf_lm(length ~ width, data = KidsFeet, color = ~biggerfoot, alpha = 0.2)
+#'   gf_lm(length ~ width, data = KidsFeet, color = ~biggerfoot, alpha = 0.2) %>%
+#'     gf_point()
 #'   gf_lm(length ~ width, data = KidsFeet, color = ~biggerfoot, fullrange = FALSE, alpha = 0.2)
+#'     gf_point()
+#'   gf_lm(length ~ width, color = ~ sex, data = KidsFeet,
+#'         formula = y ~ poly(x,2), linetype = "dashed") %>%
+#'     gf_point()
+#'   gf_lm(length ~ width, color = ~ sex, data = KidsFeet,
+#'         formula = log(y) ~ x, backtrans = exp) %>%
+#'     gf_point()
 #' }
+#' gf_lm(hwy ~ displ, data = mpg,
+#'       formula = log(y) ~ poly(x,3), backtrans = exp,
+#'      interval = "prediction", fill = "skyblue") %>%
+#'   gf_lm(
+#'      formula = log(y) ~ poly(x,3), backtrans = exp,
+#'      interval = "confidence", color = "red") %>%
+#'   gf_point()
+#'
 gf_smooth <-
   layer_factory(
     geom = "smooth",
@@ -378,6 +394,10 @@ gf_smooth <-
 
 #' @rdname gf_smooth
 #' @export
+#' @examples
+#' if (require(mosaicData)) {
+
+#' }
 
 gf_lm <-
   layer_factory(
