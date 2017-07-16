fa75001819e3588b1b9d8930a9d566c5e2edf17f
Randall Pruim <rpruim@gmail.com>
2017-07-16 10:40:32 -0400

    add example showing gf_labs() an gf_theme(); Closes #49.

--- R/gf_aux.R
+++ R/gf_aux.R
@@ -18,6 +18,23 @@
 #'
 #' @rdname gf_aux
 #' @export
+#' @examples
+#' gf_dens( ~ cesd, color = ~ substance, size = 1.5, data = HELPrct) %>%
+#' gf_labs(
+#'   title = "Center for Epidemiologic Studies Depression measure",
+#'   subtitle = "(at baseline)",
+#'   color = "Abused substance: ",
+#'   x = "CESD score",
+#'   y = "",
+#'   caption = "Source: HELPrct"
+#' ) %>%
+#'   gf_theme(theme_classic()) %>%
+#'   gf_theme(
+#'     axis.text.y = element_blank(),
+#'     legend.position = "top",
+#'     plot.title = element_text(hjust = 0.5, color = "navy"),
+#'     plot.subtitle = element_text(hjust = 0.5, color = "navy", size = 12))
+
 gf_labs <- function(object, ...) {
   object + ggplot2::labs(...)
 }
