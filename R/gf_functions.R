5a7d71755f1451dfbb2cfdc444c1b9fb4452b669
Randall Pruim <rpruim@gmail.com>
2018-01-10 15:51:35 -0500

    add example Kaplan Meier plot to gf_step() documenation

--- R/gf_functions.R
+++ R/gf_functions.R
@@ -1245,6 +1245,21 @@ gf_spoke <-
 #' if (require(mosaicData)) {
 #'   gf_step( births ~ date, data = Births78, color = ~wday)
 #' }
+#'
+#' # Roll your own Kaplan-Meier plot
+#'
+#' if (require(survival) && require(broom)) {
+#'   # fit a survival model
+#'   surv_fit <- survfit(coxph(Surv(time, status) ~ age + sex, lung))
+#'   surv_fit
+#'   # use broom::tidy() to create a tidy data frame for plotting
+#'   surv_df <- tidy(surv_fit)
+#'   head(surv_df)
+#'   # now create a plot
+#'   surv_df %>%
+#'     gf_step(estimate ~ time) %>%
+#'     gf_ribbon(conf.low + conf.high ~ time, alpha = 0.2)
+#' }
 
 gf_step <-
   layer_factory(
