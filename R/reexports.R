1acd50e6da1378aecdd858944820519b63559bb0
Randall Pruim <rpruim@gmail.com>
2018-07-31 11:53:31 -0400

    handle re-exports differntly (a la tidyverse documentation)

--- R/reexports.R
+++ R/reexports.R
@@ -1,39 +1,23 @@
 # This allows us to use these functions without attaching the packages they come from.
 
-#' Pipe operator
-#'
-#' See \code{\link[magrittr]{\%>\%}} for more details.
-#'
-#' @name %>%
-#' @rdname pipe
-#' @keywords internal
-#' @export
 #' @importFrom magrittr %>%
-#' @usage lhs \%>\% rhs
-NULL
-
-#' @rdname makeFun
-#' @importFrom mosaicCore makeFun df_stats na.warn
-#' @inherit mosaicCore::makeFun
 #' @export
-makeFun <- mosaicCore::makeFun
+magrittr::`%>%`
 
-utils::globalVariables(c("stat", "value"))
-
-#' Calculate statistics on a variable
-#'
-#' @rdname df_stats
-#' @inherit mosaicCore::df_stats
-#' @inheritParams mosaicCore::df_stats
-#' @importFrom tidyr gather
-#' @importFrom dplyr %>% bind_rows
-#' @importFrom rlang is_character f_rhs eval_tidy quos
-#' @importFrom stats as.formula na.exclude
-
+#' @importFrom mosaicCore makeFun
 #' @export
-df_stats <- mosaicCore::df_stats
+mosaicCore::makeFun
 
-#' @rdname na.warn
-#' @inherit mosaicCore::na.warn
+# utils::globalVariables(c("stat", "value"))
+
+#' @importFrom mosaicCore df_stats
+# @importFrom tidyr gather
+# @importFrom dplyr %>% bind_rows
+# @importFrom rlang is_character f_rhs eval_tidy quos
+# @importFrom stats as.formula na.exclude
 #' @export
-na.warn <- mosaicCore::na.warn
+mosaicCore::df_stats
+
+#' @importFrom mosaicCore na.warn
+#' @export
+mosaicCore::na.warn
